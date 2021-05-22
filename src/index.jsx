1b1465a4c93a1e19dddce11402c0b5545bfb7f74
Sabina Dědečková <sdmsi666@gmail.com>
2021-05-22 11:57:37 +0200

    routování

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,29 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import { render } from 'react-dom';
 import './style.css';
 import Header from './Header';
 import Footer from './Footer';
 
 const App = () => (
-  <>
-    <Header />
-    <Footer />
-  </>
+  <Router>
+    <Link to="/">Home</Link>
+    <Link to="/reservation">Reservation</Link>
+
+    <Switch>
+      <Route path="/" exact>
+        <Header />
+        <h2>1.stranka</h2>
+        <Footer />
+      </Route>
+
+      <Route path="/reservation" exact>
+        <Header />
+        <h2>2.stranka</h2>
+        <Footer />
+      </Route>
+    </Switch>
+  </Router>
 );
 
 render(<App />, document.querySelector('#app'));
